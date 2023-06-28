831f2d82c0a13be0eca4c57cdb2ba5974f4768a5
razib121 <mhrazib121@gmail.com>
2023-06-28 07:09:00 +0600

    countdown added

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Countdown from "@/components/Countdown";
+
+const CounterUpTrigger = () => {
+  const targetDate = new Date("2024-12-12");
+  return (
+    <main className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="bg-green-500 py-12 px-8 md:py-20 md:px-16 w-fit lg:w-full max-w-6xl mx-auto rounded-[32px] flex justify-center items-center ">
+        <Countdown targetDate={targetDate} />
+      </div>
+    </main>
+  );
+};
+
+export default CounterUpTrigger;
